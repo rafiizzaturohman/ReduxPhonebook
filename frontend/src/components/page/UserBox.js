6f276cac61df718ab5b3ddc3b451a4f9ba38b85d
AiceKerime <rafiizzaturohman06@gmail.com>
2022-12-31 20:36:14 +0700

    DONE

--- frontend/src/components/page/UserBox.js
+++ frontend/src/components/page/UserBox.js
@@ -6,7 +6,7 @@ export default function UserBox() {
         <div>
             <div className='grid gap-8 my-28 mx-20 md:grid-cols-none xl:grid-cols-2'>
                 {/* CARD FORM START */}
-                <div className='sticky'>
+                <div>
                     <div className='shadow-2xl shadow-slate-300 bg-white/80 rounded-lg'>
                         <div className='container py-16 px-24 space-y-10'>
                             <UserForm />
@@ -17,7 +17,7 @@ export default function UserBox() {
 
                 {/* CARD LIST START */}
                 <div>
-                    <div className=''>
+                    <div>
                         <div className='bg-gradient-to-tr from-blue-700 to-blue-500 px-8 py-1 rounded-md shadow-md'>
                             <h1 className='text-3xl text-white font-bold tracking-wide'>Phonebook App</h1>
                         </div>
